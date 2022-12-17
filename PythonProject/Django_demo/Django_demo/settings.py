7fc16c6d92fe10e99d233ef87910669c056597b1
Inclelee <inclelee@163.com>
2022-12-17 17:12:12 +0800

    测试提交

--- PythonProject/Django_demo/Django_demo/settings.py
+++ PythonProject/Django_demo/Django_demo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# 当前项目的绝对路径
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -29,7 +30,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 定义应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
