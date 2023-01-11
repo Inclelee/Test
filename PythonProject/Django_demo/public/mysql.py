9335171243aca01e9d2e6bf9fdfd897d2dea0ed5
Inclelee <inclelee@163.com>
2023-01-11 11:36:25 +0800

    测试配置角色等级记录

--- PythonProject/Django_demo/public/mysql.py
+++ PythonProject/Django_demo/public/mysql.py
@@ -1,18 +1,21 @@
 import pymysql
 
 # test
-# host = '49.235.204.77'
-# user = 'tester'
-# password = 'Dr6getstqghsfDxcmQ'
-# database = 'dlcenter_sdk'
-# port = 4580
+host = '49.235.204.77'
+user = 'tester'
+password = 'Dr6getstqghsfDxcmQ'
+database = 'dlcenter_role_report'
+port = 4580
+
+# dlcenter_sdk
+# dlcenter_role_report
 
 # localhost
-host = 'localhost'
-user = 'root'
-password = '123456'
-database = 'test_plat'
-port = 3306
+# host = 'localhost'
+# user = 'root'
+# password = '123456'
+# database = 'test_plat'
+# port = 3306
 
 db = pymysql.connect(host=host,
                      user=user,
@@ -25,6 +28,7 @@ cursor = db.cursor()
 
 def update_sql(table, key_value, condition='1'):
     sql = 'update %s set %s where %s' % (table, key_value, condition)
+    print(sql)
     cursor.execute(sql)
     db.commit()
     print(cursor.rowcount, "record(s) affected")
@@ -65,11 +69,10 @@ def execute_sql(sql):
 
 
 # 测试代码
-# 查询身份证所在
 if __name__ == '__main__':
     table = ''
-    test_flag = 1
-    if test_flag == 1:
+    test_flag = 0
+    if test_flag == 1:      # 查询身份证所在表
         sql_user_table = 'SELECT table_name ' \
                          'FROM information_schema.STATISTICS ' \
                          'WHERE table_schema = \'dlcenter_sdk\' ' \
@@ -91,8 +94,8 @@ if __name__ == '__main__':
                     print(res_search_user)
             # print('')
 
-    test_flag = 1
-    if test_flag == 1:
+    test_flag = 0
+    if test_flag == 1:      # 根据身份证查询平台账号id
         table = 'plat_user_login_hh'
         fild = 'id'
         # condition = 'plat_user_name = \'13553349001\' ' \
@@ -102,3 +105,20 @@ if __name__ == '__main__':
                                 fild,
                                 condition)
         print(res_select)
+
+    test_flag = 1
+    if test_flag == 1:
+        print('test-配置等级')
+
+        level = 500
+        table_update = 'dlcenter_role_report.role_upgrade_42 AS ru,' \
+                'dlcenter_log.statis_role_183 AS sr,' \
+                'dlcenter_sdk.account_red_packet_bind_role AS arpbr '
+        data_key_value = 'arpbr.`level` = %i,sr.`level` = %i,ru.role_level = %i' % (level, level, level)
+        condition_update = 'arpbr.role_id = \'hh114005\' ' \
+                    'AND arpbr.role_id = ru.role_id ' \
+                    'AND arpbr.role_id = sr.role_id ' \
+                    'AND arpbr.app_id = 42 ' \
+                    'AND arpbr.game_id = sr.game_id ' \
+                    'AND arpbr.app_id = ru.app_id;'
+        update_sql(table_update, data_key_value, condition_update)
